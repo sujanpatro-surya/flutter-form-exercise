2f95c7e43cee5136cf395a6ab1262baae1038ba6
sujanpatro-surya <sujan.kp@surya-digital.com>
2023-04-18 10:31:23 +0530

    Removed use of not configured appBarTheme

--- lib/theme/main_theme.dart
+++ lib/theme/main_theme.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_exercise/theme/colors.dart';
 import 'text_theme.dart';
-import 'app_bar_theme.dart';
 
 ThemeData mainTheme = ThemeData(
   brightness: Brightness.light,
   scaffoldBackgroundColor: Colors.white,
   colorSchemeSeed: AllAppColors.primaryLight,
   fontFamily: 'Inter',
-  appBarTheme: appBarTheme,
   textTheme: textTheme()
 );
