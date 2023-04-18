a0044270cb44d30c701c5b0b86fa577b4083fbee
sujanpatro-surya <sujan.kp@surya-digital.com>
2023-04-18 10:35:33 +0530

    Used correct primary values for the custom defined Material Colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AllAppColors {
-  static const MaterialColor labelLight = MaterialColor(300, {
+  static const MaterialColor labelLight = MaterialColor(0xFF36383A, {
     100: Color(0xFFC5C5C7),
     200: Color(0xFF8A8A8E),
     300: Color(0xFF36383A),
     400: Color(0xFF000000),
   });
-  static const MaterialColor backgroundLight = MaterialColor(400, {
+  static const MaterialColor backgroundLight = MaterialColor(0xFFFFFFFF, {
     100: Color(0xFFEEEEF3),
     200: Color(0xFFF2F2F7),
     300: Color(0xFFF6F6FB),
     400: Color(0xFFFFFFFF),
   });
-  static const MaterialColor primaryLight = MaterialColor(300, {
+  static const MaterialColor primaryLight = MaterialColor(0xFF6A39F1, {
     100: Color(0xFFF4F1FE),
     200: Color(0xFFBEA9F9),
     300: Color(0xFF6A39F1),
     400: Color(0xFF320B9E),
   });
-  static const MaterialColor outlineLight = MaterialColor(300, {
+  static const MaterialColor outlineLight = MaterialColor(0xFFD1D1D6, {
     100: Color(0xFFF2F2F7),
     200: Color(0xFFE5E5EA),
     300: Color(0xFFD1D1D6),
     400: Color(0xFF8E8E93),
   });
-  static const MaterialColor errorLight = MaterialColor(300, {
+  static const MaterialColor errorLight = MaterialColor(0xFFFF3B30, {
     100: Color(0xFFFFF1F0),
     200: Color(0xFFFFAAA5),
     300: Color(0xFFFF3B30),
