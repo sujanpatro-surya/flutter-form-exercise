2df02eacf0c8676f6303d66237e3e5c5a4bd1b21
sujanpatro-surya <sujan.kp@surya-digital.com>
2023-04-19 17:02:54 +0530

    Removed unnecessary dropDownMenuTheme

--- lib/theme/dropdown_menu_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_form_exercise/theme/text_theme.dart';
-
-DropdownMenuThemeData dropdownMenuThemeData = DropdownMenuThemeData(
-  textStyle: textTheme.bodyMedium
-);
