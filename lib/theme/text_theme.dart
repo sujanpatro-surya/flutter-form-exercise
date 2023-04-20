6910c869a4a7cb4ca32c3396ad22dc7156cee772
sujanpatro-surya <sujan.kp@surya-digital.com>
2023-04-20 11:29:56 +0530

    Used .apply() on TextTheme class to apply font colors on the all textStyles

--- lib/theme/text_theme.dart
+++ lib/theme/text_theme.dart
@@ -4,24 +4,21 @@ import 'colors.dart';
 /// The referenced design (Figma) is based on the Unity Style Typography. But
 /// the one we are supposed to follow is Material Design Typography. Each [TextStyle]
 /// defined below corresponds to the matching text style defined in Unity.
-const TextTheme textTheme = TextTheme(
+final TextTheme textTheme = const TextTheme(
   /// Body/B1
   bodyLarge: TextStyle(
-    color: AppColors.label,
     fontSize: 16,
     letterSpacing: 0.2,
   ),
 
   /// Body/B2
   bodyMedium: TextStyle(
-    color: AppColors.label,
     fontSize: 14,
     letterSpacing: 0.22
   ),
 
   /// Button/BT2
   labelLarge: TextStyle(
-    color: AppColors.label,
     fontWeight: FontWeight.bold,
     fontSize: 14,
     letterSpacing: 0.32
@@ -29,7 +26,6 @@ const TextTheme textTheme = TextTheme(
 
   /// Button/BT3
   labelMedium: TextStyle(
-    color: AppColors.label,
     fontWeight: FontWeight.bold,
     fontSize: 12,
     letterSpacing: 0.35
@@ -37,7 +33,6 @@ const TextTheme textTheme = TextTheme(
 
   /// Small/S1
   headlineSmall: TextStyle(
-    color: AppColors.label,
     fontWeight: FontWeight.bold,
     fontSize: 16,
     letterSpacing: 0.2,
@@ -45,8 +40,7 @@ const TextTheme textTheme = TextTheme(
 
   /// Caption/C1
   bodySmall: TextStyle(
-    color: AppColors.label,
     fontSize: 12,
     letterSpacing: 0.3,
   )
-);
+).apply(displayColor: AppColors.label, bodyColor: AppColors.label);
